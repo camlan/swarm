2a1e4bdbe54bc9ca9552a83c5203eb8acbd329b8
Sylwester Lewandowski <sylwester.l@gmail.com>
2025-02-04 00:03:53 +0100

    failing test added

--- ant/cuda/cuda_test.go
+++ ant/cuda/cuda_test.go
@@ -8,7 +8,6 @@ import (
 )
 
 func TestFindShortestPathSanity(t *testing.T) {
-
 	// given
 	distanceMap := [][]float64{
 		{0.0, 3.0, 5.0, 4.0},
@@ -27,9 +26,10 @@ func TestFindShortestPathSanity(t *testing.T) {
 	}
 }
 
-func TestFindShortestPath(t *testing.T) {
+func TestFindShortestPathToFix(t *testing.T) {
 	// given
-	distancesToVerify := internal.DistancesToVerifyFeed()
+	//distancesToVerify := internal.DistancesToVerifyFeed()
+	distancesToVerify := make(map[float64][][]float64)
 	distancesToVerify[1068] = file.GenerateCityMatrix("..\\data\\wg59_dist.txt")
 
 	for expected, distanceMap := range distancesToVerify {
@@ -37,7 +37,7 @@ func TestFindShortestPath(t *testing.T) {
 		foundPath := FindShortestPath(distanceMap)
 		//then
 		if !internal.IsInRange(expected, foundPath.Distance) {
-			t.Errorf("Expected distance %v, got:%v", expected, foundPath.Distance)
+			t.Errorf("Expected distance %v, got:%v, for sequence: ", expected, foundPath.Distance)
 		}
 	}
 }
